c54b516986975978869e0ab2352a25da07aea801
gaeguriBanchan <rlawowls93@gamil.com>
2023-08-18 14:10:46 +0900

    기본 UI작업

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -10,15 +10,99 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          '폰트확인',
-          style: TextStyle(
-            fontFamily: 'sunflower',
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('asset/img/home.png'),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          bottom: false,
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                Flexible(
+                  flex: 5,
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                  ),
+                ),
+                const Flexible(
+                  flex: 50,
+                  child: _MainPart(),
+                ),
+                Flexible(
+                  flex: 45,
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class _MainPart extends StatelessWidget {
+  const _MainPart();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        const Text(
+          'U&I',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'parisienne',
+            fontSize: 80.0,
+          ),
+        ),
+        const Column(
+          children: [
+            Text(
+              '우리 처음 만난날',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'sunflower',
+                fontSize: 30.0,
+              ),
+            ),
+            Text(
+              '2023.03.29',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'sunflower',
+                fontSize: 20.0,
+              ),
+            ),
+          ],
+        ),
+        IconButton(
+          iconSize: 60.0,
+          onPressed: () {},
+          icon: const Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),
+        ),
+        const Text(
+          'D+1',
+          style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'sunflower',
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700),
+        )
+      ],
     );
   }
 }
